20649c1a7b5bdfcdd14b9e4930ec15f05ed0c91e
durkesh-datasci <nikola.anuja@gmail.com>
2024-09-27 21:31:37 +0530

    Added code to print heart shape in java.

---
+++ heart_pattern.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class heart_pattern{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int st=n/2;
+        for(int i=st+1;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<i;k++){
+                System.out.print("* ");
+            }
+            for(int s=0;s<(2*(n-i));s++){
+                System.out.print(" ");
+            }
+            
+            for(int k=0;k<i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=2*n-1;i>=0;i--){
+            if(i%2==1){
+                for(int j=0;j<2*n-i;j++){
+                    System.out.print(" ");
+                }
+                for(int k=i;k>0;k--){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
