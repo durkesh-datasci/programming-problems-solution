9fc1ed8ab51dd7efbbfb30373a8640f6ebd82182
durkesh-datasci <nikola.anuja@gmail.com>
2025-02-01 15:58:43 +0530

    Added code for returning the given array is Special array or not - Leetcode problem

---
+++ special_array.java
@@ -0,0 +1,13 @@
+public class special_array {
+    public boolean isArraySpecial(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]%2== nums[i+1]%2){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        //your main class
+    }
+}
